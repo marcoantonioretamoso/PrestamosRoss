c038ed9fd7cd9605138aad3cae70d4b7a541246d
Marco Retamoso <retamosorodriguezmarcoantonio@gmail.com>
2025-11-06 21:21:30 -0400

    add migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
+            $table->string('image_qr')->nullable();
+            $table->string('instance_wsp')->nullable();
+            $table->unsignedBigInteger('selected_account')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
